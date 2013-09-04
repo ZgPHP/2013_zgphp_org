1e78ecf59db09ed328fbca42b279ea73446d1528
Ivan Habunek <ivan.habunek@gmail.com>
2013-09-04 11:27:02 +0200

    Removed unused javascript

--- index.php
+++ index.php
@@ -248,18 +248,6 @@ require 'data.php';
 
 <!-- Load JS here for greater good =============================-->
 <script src="js/jquery-1.8.3.min.js"></script>
-<script src="js/jquery-ui-1.10.3.custom.min.js"></script>
-<script src="js/jquery.ui.touch-punch.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/bootstrap-select.js"></script>
-<script src="js/bootstrap-switch.js"></script>
-<script src="js/flatui-checkbox.js"></script>
-<script src="js/flatui-radio.js"></script>
-<script src="js/jquery.tagsinput.js"></script>
-<script src="js/jquery.placeholder.js"></script>
-<script src="js/jquery.stacktable.js"></script>
-<script src="http://vjs.zencdn.net/c/video.js"></script>
-<script src="js/application.js"></script>
 
 <!-- The Map -->
 <script src="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js"></script>
