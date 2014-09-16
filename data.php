084a8c85ea16f32097823aeb735c79ec802eced4
Miro Svrtan <miro@mirosvrtan.me>
2014-09-16 19:41:34 +0200

    Schedule changes

--- data.php
+++ data.php
@@ -101,40 +101,47 @@ $arrTalks = array(
         'slides' => null,
     ),
     array(
-        'title' => 'Lunch break',
-        'abstract' => '',
-        'time' => '13:55'
+        'person' => 'Bruno Škvorc',
+        'title' => 'Getting paid to play with new technologies & the PHP community from an editor\'s point of view',
+        'abstract' => 'A short introduction into the world of tutoring and general writing for the greater good of the PHP community and, of course, profit. Learn some interesting insights about the community at large and find out about the educational side of PHP, passive income, and monetization of knowledge. As a bonus, find out some gender, age, location and other stats of the readership of one of the most popular online PHP magazines and play with the raw data yourself.',
+        'time' => '14:00',
+        'slides' => null,
     ),
     array(
-        'person' => 'Ivo Lukač',
-        'title' => 'The power of ESI and HTTP Cache for performant page delivery',
-        'abstract' => 'For web sites with frequent content changes, like news portals, an optimal cache strategy is crucial for serving millions of page views. To just cache every page statically is usually not an option as editors want to see the changes immediately. With the new version of eZ Publish CMS, which  is based on Symfony PHP framework, content caching is based on HTTP Cache. This enables much tighter integration with reverse proxies like Varnish and better control of cached pages. Also, Symfony provides ESI capabilities for controlling reverse proxy cache even on block level so invalidating a whole page is not expensive.',
-        'time' => '14:55'
+        'title' => 'Lunch break',
+        'abstract' => '',
+        'time' => '14:30'
+    ),
+    array(
+        'person' => 'Antonio Perić',
+        'title' => 'Symfony2 + Angular.js',
+        'abstract' => 'Forget about classic website where UX is not so important. We are living in time where usability is one of the important thing if you are building some business client oriented web service. How to connect Symfony2 as backend and AngularJS as frontend solution? What are best practices? What are disadvantageous? How to take best from both worlds? These are topics I will cover in my talk with real examples.',
+        'time' => '15:30'
     ),
     array(
         'person' => 'Srđan Vranac',
         'title' => 'Shifting gears with Gearman',
         'abstract' => 'Everyone starts with a simple one-machine setup, running PHP(or whatever else you fancy), MySQL/PostgreSql and Apache/Nginx. Sooner or later that will not be enough, and you will be faced with some important architecture decisions. This talk will show you how you can optimize and increase the performance of your application by distributing tasks in an simple and affordable way.',
-        'time' => '15:30',
+        'time' => '16:05',
         'slides' => null
     ),
     array(
-        'title' => 'TBD',
-        'abstract' => '',
-        'time' => '16:35',
-        'slides' => null,
+        'person' => 'Ivo Lukač',
+        'title' => 'The power of ESI and HTTP Cache for performant page delivery',
+        'abstract' => 'For web sites with frequent content changes, like news portals, an optimal cache strategy is crucial for serving millions of page views. To just cache every page statically is usually not an option as editors want to see the changes immediately. With the new version of eZ Publish CMS, which  is based on Symfony PHP framework, content caching is based on HTTP Cache. This enables much tighter integration with reverse proxies like Varnish and better control of cached pages. Also, Symfony provides ESI capabilities for controlling reverse proxy cache even on block level so invalidating a whole page is not expensive.',
+        'time' => '17:00'
     ),
     array(
         'person' => 'Ross Tuck',
         'title' => 'HTTP & Your Angry Dog',
         'abstract' => 'What is an etag, exactly? What\'s all that stuff in the Accept header? And what the heck does a Vary header do anyways?! Web developers use HTTP everyday but most of us don\'t know how to get the most out of it. This talk goes past memorizing status codes (although we\'ll see those too) and teaches how to get the most out of every request and response.',
-        'time' => '17:25',
+        'time' => '17:35',
         'slides' => null,
     ),
     array(
         'title' => 'Closing and giveaway',
         'abstract' => '',
-        'time' => '18:15'
+        'time' => '18:25'
     ),
 );
 
